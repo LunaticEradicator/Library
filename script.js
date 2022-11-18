9c7515ad0bbc14ff7b31dbd2869c876fe4a92849
YeduKrishnan A <kuttuzz17@gmail.com>
2022-11-18 12:11:15 +0530

    Clean-Up .js file for better readability

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const addBookBtn = document.querySelector(".addBookBtn");
 const display = document.querySelector(".display");
+const addBookBtn = document.querySelector(".addBookBtn");
 const submitBtn = document.querySelector(".submitBtn");
 const modal = document.querySelector(".modal");
 const readBtn = document.querySelector('.readBtn');
@@ -11,15 +11,6 @@ const authorInput = document.querySelector(".authorInput");
 const pageInput = document.querySelector(".pageInput");
 const ratingInput = document.querySelector(".ratingInput");
 const readStatusInput = document.querySelector(".readStatusInput");
-
-
-
-let myLibrary = [
-    // { name: "How to be Noob ", author: "Yedu", year: 2017, rating: 1 },
-    // { name: "How to be Pro ", author: "Kannan", year: 2009, rating: 7 },
-    // { name: "How to be God ", author: "Messi", year: 2022, rating: 10 },
-];
-
 
 
 function Book(name, author, page, rating, readStatus) {
@@ -34,190 +25,193 @@ Book.prototype.addBook = function () {
     return `Name : ${this.name} Author : ${this.author} Year : ${this.page} Rating : ${this.rating} ReadStats : ${this.readStatus}`
 }
 
-const bookOne = new Book("Hard Work and Dedication", "CR7", 1990, 10);
-const bookTwo = new Book("Dive and ShowBot", "NY9", 2000, 8);
-const bookThree = new Book("How to be a GOAT", "Messi", 2012, 10);
+// Each Time Submit Button is Pressed a new object will be created using object constructor [submitBtn function is invoked]
+let myLibrary = [
+    // { name: "How to be Noob ", author: "Yedu", year: 2017, rating: 1 }
+];
 
-// myLibrary.splice(-1, 0, bookOne);
-// myLibrary.push(bookTwo);
-// myLibrary.push(bookThree);
-
-function displayingBookToLibrary() {
+//Loop through every item in the array myLibrary
+function displayBookToLibrary() {
     for (let library of myLibrary) {
-        displayStyle(library);
+        addBook(library);
     }
 }
 
-
-
-
-function displayStyle(library) {
+function addBook(library) {
+    //---Created DOM Element---//
+    //Main Element
     const eachBookStyle = document.createElement('div');
-    const hideDiv = document.createElement('div');
-    hideDiv.classList.add("hideDiv");
 
+    //EachBookStyle Section One
+    const iconSection = document.createElement('div');
+    const deleteBtn = document.createElement('button');
+    const minimizeIcon = document.createElement('button');
 
-
-
-
+    // EachBookStyle Section Two
     const nameDiv = document.createElement('div');
+    const nameSpan = document.createElement('span');
+
+    //EachBookStyle Section Three
+    const hideSection = document.createElement('div');
     const authorDiv = document.createElement('div');
     const pageDiv = document.createElement('div');
     const ratingDiv = document.createElement('div');
-
-    const nameSpan = document.createElement('span');
     const authorSpan = document.createElement('span');
     const pageSpan = document.createElement('span');
     const ratingSpan = document.createElement('span');
+    const readBtnDivDisplay = document.createElement('div');
+    const readBtnDisplay = document.createElement('button');
 
-    const readBtn = document.createElement('button');
-    const readBtnDiv = document.createElement('div');
+    const showMoreDiv = document.createElement('div');
+    const showMoreIcon = document.createElement('div');
 
-    const deleteDiv = document.createElement('div');
-    deleteDiv.classList.add("deleteDiv")
-    eachBookStyle.append(deleteDiv);
 
-    const deleteBtn = document.createElement('button');
+
+    //---Classlist---//
+    //EachBookStyle Section One
+    iconSection.classList.add("iconSection")
     deleteBtn.textContent = "X";
     deleteBtn.classList.add("deleteBtn")
-    deleteDiv.append(deleteBtn);
+    minimizeIcon.textContent = "";
+    minimizeIcon.classList.add("minimizeIcon")
 
-    const readIcon = document.createElement('button');
-    readIcon.textContent = "";
-    readIcon.classList.add("readIcon")
-    deleteDiv.append(readIcon);
-
+    //EachBookStyle Section Two
     nameDiv.textContent = "Book";
     nameSpan.textContent = ` ${library.name}`;
 
+    //EachBookStyle Section Three
     authorDiv.textContent = "Author";
     authorSpan.textContent = ` ${library.author}`;
-
     pageDiv.textContent = "Pages";
     pageSpan.textContent = ` ${library.page}`;
-
     ratingDiv.textContent = "Rating";
+
     ratingSpan.textContent = ` ${library.rating}`;
+    readBtnDisplay.textContent = `${library.readStatus}`;
 
-    readBtn.textContent = `${library.readStatus}`;
 
+    //---DOM ClassList---//
+    //Main Section 
     eachBookStyle.classList.add("eachBookStyle");
 
+    //EachBookStyle Section Two
     nameDiv.classList.add("nameDiv");
     nameSpan.classList.add("nameSpan");
 
+    //EachBookStyle Section Three
+    hideSection.classList.add("hideSection");
     authorDiv.classList.add("authorDiv");
     authorSpan.classList.add("authorSpan");
-
     pageDiv.classList.add("yearDiv");
     pageSpan.classList.add("yearSpan");
-
     ratingDiv.classList.add("ratingDiv");
     ratingSpan.classList.add("ratingSpan");
 
-    readBtnDiv.classList.add("readBtnDiv");
-    readBtn.classList.add("readBtn");
+    readBtnDivDisplay.classList.add("readBtnDivDisplay");
+    readBtnDisplay.classList.add("readBtnDisplay");
 
-    if (readBtn.textContent === "Read") {
-        readBtn.classList.remove("readStatus");
+    showMoreDiv.classList.add("showMoreDiv");
+    showMoreDiv.classList.add("showMoreDivHide");
+    showMoreIcon.classList.add("showMoreIcon");
+
+    //Toggle readBtnOnOff on EachBookStyle
+    if (readBtnDisplay.textContent === "Read") {
+        readBtnDisplay.classList.remove("notReadStatus");
         eachBookStyle.style.border = "3px solid rgb(77, 189, 62)";
     }
     else {
-        readBtn.classList.add("readStatus");
+        readBtnDisplay.classList.add("notReadStatus");
         eachBookStyle.style.border = "3px solid red";
     }
 
+    //---Appended DOM Element---//
+    //Section One
+    eachBookStyle.append(iconSection);
+    iconSection.append(deleteBtn);
+    iconSection.append(minimizeIcon);
 
+    //Section Two
     eachBookStyle.append(nameDiv);
     eachBookStyle.append(nameSpan);
 
-    hideDiv.append(authorDiv);
-    hideDiv.append(authorSpan);
+    //Section Three
+    readBtnDivDisplay.append(readBtnDisplay);
+    hideSection.append(authorDiv);
+    hideSection.append(authorSpan);
+    hideSection.append(pageDiv);
+    hideSection.append(pageSpan);
+    hideSection.append(ratingDiv);
+    hideSection.append(ratingSpan);
+    hideSection.append(readBtnDivDisplay);
+    showMoreDiv.append(showMoreIcon);
 
-    hideDiv.append(pageDiv);
-    hideDiv.append(pageSpan);
-
-    hideDiv.append(ratingDiv);
-    hideDiv.append(ratingSpan);
-
-    hideDiv.append(readBtnDiv);
-    readBtnDiv.append(readBtn);
-
-    const hideDivImage = document.createElement('div');
-    const hideDivImageBackground = document.createElement('div');
-    // hideDivImageBackground.textContent = "nice";
-    hideDivImage.classList.add("hideDivImage");
-    hideDivImage.classList.add("hideDivStatus");
-    hideDivImageBackground.classList.add("hideDivImageBackground");
-    hideDivImage.append(hideDivImageBackground);
-
-    eachBookStyle.append(hideDiv);
-    eachBookStyle.append(hideDivImage);
+    //Main Section 
+    eachBookStyle.append(hideSection);
+    eachBookStyle.append(showMoreDiv);
     display.append(eachBookStyle);
 
-    readBtn.addEventListener("click", readBtnStatus)
-    function readBtnStatus() {
-        console.log("CLICKED")
-        if (readBtn.textContent === "Read") {
-            readBtn.textContent = "Not Read";
-            readBtn.classList.toggle("readStatus");
+    //---Event Listener and Function---//
+    readBtnDisplay.addEventListener("click", readBtnDisplayStatus)
+    minimizeIcon.addEventListener("click", minimizeIconStatus)
+    showMoreIcon.addEventListener("mousedown", showMoreIconAnimation)
+    removeBook(deleteBtn, eachBookStyle);
+
+    function readBtnDisplayStatus() {
+        // console.log("CLICKED")
+        if (readBtnDisplay.textContent === "Read") {
+            readBtnDisplay.textContent = "Not Read";
+            readBtnDisplay.classList.toggle("notReadStatus");
             eachBookStyle.style.border = "3px solid red";
-            // eachBookStyle.style.boxShadow = "1px 1px 30px 2px rgb(224, 0, 0)";
         }
         else {
-            readBtn.textContent = "Read";
-            readBtn.classList.toggle("readStatus");
+            readBtnDisplay.textContent = "Read";
+            readBtnDisplay.classList.toggle("notReadStatus");
             eachBookStyle.style.border = "3px solid rgb(77, 189, 62)";
-            // eachBookStyle.style.boxShadow = " 1px 1px 30px 2px rgb(0, 224, 49)";
         }
         event.preventDefault();
     }
-
-    removeBook(deleteBtn, eachBookStyle);
-
-    readIcon.addEventListener("click", e => {
-        readIcon.classList.toggle("readIconStatus");
-        hideDiv.classList.toggle("hideDivStatus");
-        eachBookStyle.classList.toggle("eachBookStyleStatus");
-        hideDivImage.classList.toggle("hideDivStatus");
-        // eachBookStyle.style.height = "100%";
-    })
-
-    hideDivImageBackground.addEventListener("click", e => {
-        readIcon.classList.toggle("shakeAnimation");
-    })
-
+    function minimizeIconStatus() {
+        eachBookStyle.classList.toggle("eachBookStyleHeight");
+        minimizeIcon.classList.toggle("minimizeIconOn");
+        hideSection.classList.toggle("showMoreDivHide");
+        showMoreDiv.classList.toggle("showMoreDivHide");
+    }
+    function showMoreIconAnimation() {
+        // console.log("added");
+        minimizeIcon.classList.add("shakeAnimation");
+        setTimeout(toggleAnimation, 500);
+        function toggleAnimation() {
+            minimizeIcon.classList.remove("shakeAnimation");
+            // console.log("removed");
+        }
+    }
 }
-
 
 submitBtn.addEventListener("click", submitPress);
 readBtn.addEventListener("click", readBtnStatus);
 addBookBtn.addEventListener("click", ShowModal)
-
-
 
 function ShowModal() {
     modal.showModal();
 }
 
 function submitPress() {
-    const news = new Book(`${nameInput.value}`, `${authorInput.value}`, `${pageInput.value}`, `${ratingInput.value}`, `${readStatusInput.textContent}`);
-    console.log(nameInput.value.length)
-    // if (nameInput.value === "" || authorInput.value === "" || yearInput.value === "" || ratingInput.value === "") {
-    //html required field will popup
-    // }
-    // else {
-    event.preventDefault();
-    console.log(news);
-    myLibrary.push(news);
-    modal.close();
-    // }
-    displayingBookToLibrary();
-
-    myLibrary.pop(news);
+    const addNewBook = new Book(`${nameInput.value}`, `${authorInput.value}`, `${pageInput.value}`, `${ratingInput.value}`, `${readStatusInput.textContent}`);
+    if (nameInput.value === "" || authorInput.value === "" || pageInput.value === "" || ratingInput.value === "") {
+        //HTML required field will show
+    }
+    else if (ratingInput.value > 10) {
+        error();
+    }
+    else {
+        event.preventDefault();
+        console.log(addNewBook);
+        myLibrary.push(addNewBook);
+        modal.close();
+    }
+    displayBookToLibrary();
+    myLibrary.pop(addNewBook); // So it won't be Re-added again
     clearValueAfterSubmit()
-    // display.style.border = "2px solid rgb(133, 130, 130)";
-
 }
 
 function clearValueAfterSubmit() {
@@ -234,12 +228,12 @@ function readBtnStatus() {
 
     if (readBtn.textContent === "Read") {
         readBtn.textContent = "Not Read";
-        readBtn.classList.add("readStatus");
+        readBtn.classList.add("notReadStatus");
         console.log("Red");
     }
     else {
         readBtn.textContent = "Read";
-        readBtn.classList.remove("readStatus");
+        readBtn.classList.remove("notReadStatus");
         console.log("Green");
     }
     event.preventDefault();
@@ -252,26 +246,16 @@ function removeBook(deleteBtn, eachBookStyle) {
 }
 
 
-// let ifRead = false;
-// function readBtnStatus() {
-//     if (!ifRead) {
-//         ifRead = true;
-//         readBtn.textContent = "Not Read";
-//         readBtn.classList.add("readStatus");
-//         console.log("Red")
-//     }
-//     else {
-//         ifRead = false;
-//         readBtn.textContent = "Read";
-//         readBtn.classList.remove("readStatus");
-//         console.log("Green")
-//     }
-//     event.preventDefault();
-// }
+// const bookOne = new Book("Hard Work and Dedication", "CR7", 1990, 10);
+// const bookTwo = new Book("Dive and ShowBot", "NY9", 2000, 8);
+// const bookThree = new Book("How to be a GOAT", "Messi", 2012, 10);
 
-// for (let library of myLibrary) {
-//     const mainDiv = document.createElement('div');
-//     mainDiv.classList.add("eachBookStyle");
-//     mainDiv.textContent = `Name : ${library.name} Author : ${library.author} Year : ${library.year} Rating : ${library.rating}`;
-//     display.append(mainDiv);
-// }
+// myLibrary.splice(-1, 0, bookOne);
+// myLibrary.push(bookTwo);
+// myLibrary.push(bookThree);
+
+// let myLibrary = [
+    // { name: "How to be Noob ", author: "Yedu", year: 2017, rating: 1 },
+    // { name: "How to be Pro ", author: "Kannan", year: 2009, rating: 7 },
+    // { name: "How to be God ", author: "Messi", year: 2022, rating: 10 },
+// ];
